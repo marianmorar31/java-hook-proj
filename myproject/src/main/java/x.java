4cf797e8e6023247eff95de75d28a38a95e6bb34
MorarMarian <marian.morar@flutteruki.com>
2024-09-12 11:01:31 +0300

    awdhijawbdw

--- myproject/src/main/java/x.java
+++ myproject/src/main/java/x.java
@@ -1,5 +1,5 @@
 public class X {
     public static void main(String[] args) {
-        System.out.println("Hello from X dawdawd!");
+        System.out.println("Hello frasdahsdubasdom X dawdawd!");
     }
 }
