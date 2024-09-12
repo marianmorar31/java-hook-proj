b916af466bb31be1589b00235d8a1b7d4928d8a9
MorarMarian <marian.morar@flutteruki.com>
2024-09-12 12:27:31 +0300

    feat(G10-215): add login functionality

--- myproject/src/main/java/com/example/App.java
+++ myproject/src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Woawdawdawdawradwawdawdawdawdadawdawdald! awdawda" );
+        System.out.println( "Hello Woawdaasdasdasdawdawdawradwawdawdawdawdadawdawdald! awdawda" );
     }
 }
